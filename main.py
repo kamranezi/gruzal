a0959e4bbaf898a15f4b791f206ba1a46340667f
kamranezi <alexrus1144@gmail.com>
2022-11-28 15:03:06 +0300

    add

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///gruzal.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
 
 
 @app.route('/')
