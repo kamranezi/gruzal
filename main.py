4174ef19c82706af695fc81790dbc0a6d362b7d1
kamranezi <alexrus1144@gmail.com>
2022-11-28 23:12:53 +0300

    add

--- main.py
+++ main.py
@@ -1,27 +1,66 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///gruzal.db'
-db = SQLAlchemy(app)
+main = Flask(__name__)
+main.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gruzal.db'
+main.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(main)
+
 
 class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), primary_key=False)
+    intro = db.Column(db.String(300), primary_key=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
-@app.route('/')
-@app.route('/home')
+@main.route('/')
+@main.route('/home')
 def index():
     return render_template("index.html")
 
 
-@app.route('/about')
+@main.route('/about')
 def about():
     return render_template("about.html")
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User page: " + name + " - " + str(id)
+
+@main.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template("posts.html", articles=articles)
+
+
+@main.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template("post_detail.html", article=article)
+
+
+
+@main.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Что-то пошло не так"
+    else:
+        return render_template("create-article.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main.run(debug=True)
